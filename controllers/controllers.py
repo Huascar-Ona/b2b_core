1c899b81470a81b348f2827c501ee9298bbe17af
root <root@usve246572.serverprofi24.com>
2017-03-17 16:34:11 -0600

    Envio desde wizard

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,44 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo import models
-import json
-import base64
-import requests
-from StringIO import StringIO
-import os
-
-TOKEN = "322027161:AAHeLdME3aTbzJByCBSBfgPyCACzW6QVKL4"
 
 class VentaTiempoAire(http.Controller):
      @http.route('/api/telegram/', auth='user', type='json', methods=['POST'], csrf=False)
      def index(self, file, filename, telegram_id, reply_markup=False):
-        token = TOKEN
         partners = http.request.env['res.partner']
         partner = partners.search([('telegram_chat_id','=', telegram_id)])
 
+        if not partner:
+            partner = partners.search([('identifier','=', telegram_id)])
+
         if partner:
-            api_url = "https://api.telegram.org/bot" + token
-
-            url = api_url + "/sendDocument"
-            params = {'chat_id':int(telegram_id)}
-        
-            file_content = base64.b64decode(file)
-            tempf = "/tmp/tmp%s"%http.request.env.uid
-            if not os.path.exists(tempf):
-                os.makedirs(tempf)
-            file_path = os.path.join(tempf, filename)
-            with open(file_path, "wb") as f:
-                f.write(file_content)
-            files = {'document': open(file_path, 'rb')}
-
-            if reply_markup:
-                params["reply_markup"] = json.dumps(reply_markup)
-                
-            r = requests.post(url, params=params, files=files)
-            
-            os.unlink(os.path.join(tempf, filename))
-            os.rmdir(tempf)
-            
+            partner.send_file(file, filename)
             return {"response": "OK"}
         else:
             return {"telegram_id": "ERROR"}
--- res_partner.py
+++ res_partner.py
@@ -1,8 +1,51 @@
-from odoo import models, fields
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+import base64
+import os
+import json
+import requests
 
 class res_partner(models.Model):
-  _name = "res.partner"
   _inherit = "res.partner"
 
-  telegram_chat_id = fields.Integer("Telegram Chat ID")
-  identifier = fields.Integer("Identificador")
+  telegram_chat_id = fields.Char("Telegram Chat ID")
+  identifier = fields.Char("Identificador alterno")
+  bot_token = fields.Char("Token del bot")  
+
+  @api.model
+  def send_file(self, file, filename, reply_markup=False):  
+    api_url = "https://api.telegram.org/bot" + self.bot_token
+
+    url = api_url + "/sendDocument"
+    params = {'chat_id':int(self.telegram_chat_id)}
+
+    file_content = base64.b64decode(file)
+    tempf = "/tmp/tmp%s"%self.env.uid
+    if not os.path.exists(tempf):
+        os.makedirs(tempf)
+    file_path = os.path.join(tempf, filename)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    files = {'document': open(file_path, 'rb')}
+
+    if reply_markup:
+        params["reply_markup"] = json.dumps(reply_markup)
+
+    r = requests.post(url, params=params, files=files)
+
+    os.unlink(os.path.join(tempf, filename))
+    os.rmdir(tempf)
+    
+    return True
+
+class wizard_send_file(models.TransientModel):
+    _name = "venta_aire.wizard_send_file"
+
+    file = fields.Binary("Archivo")
+    filename = fields.Char("Nombre del archivo")
+
+    @api.one
+    def action_send(self):
+        partner = self.env["res.partner"].browse(self.env.context.get("active_id"))
+        partner.send_file(self.file, self.filename)
+        return True
