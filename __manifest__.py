745cb5af6e17b6546d7a7a425a75aa66b7910e7b
root <root@usve246572.serverprofi24.com>
2017-03-17 16:36:26 -0600

    Cambio de nombre a b2b telegram

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "venta_tiempo_aire",
+    'name': "B2B Core",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Core para el módulo de telegram""",
 
     'description': """
         Long description of module's purpose
